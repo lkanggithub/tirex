7390d504a28d1970d70e1cdcec35f89b77af9494
Lingjun Kang <lyndon.kang@datarobot.com>
2025-07-09 17:12:23 -0400

    test

--- examples/gifteval/test_gifteval.py
+++ examples/gifteval/test_gifteval.py
@@ -1,16 +1,17 @@
 from tirex import ForecastModel, load_model
 
-model: ForecastModel = load_model("NX-AI/TiRex", device="cuda:0")
-
-
 import pandas as pd
 from gift_eval_utils import TiRexGiftEvalWrapper, evaluate_dataset, gift_eval_dataset_iter
 
-wrapped_model = TiRexGiftEvalWrapper(model)
-results = []
-for task in gift_eval_dataset_iter():
-    task_result = evaluate_dataset(wrapped_model, **task)
-    results.append(task_result)
-    print(task_result)
-results = pd.DataFrame(results)
-print(results)
+
+def test() -> None:
+    model: ForecastModel = load_model("NX-AI/TiRex", device="cuda:0")
+    wrapped_model = TiRexGiftEvalWrapper(model)
+    results = []
+    for task in gift_eval_dataset_iter():
+        print(f">>>>>>>>>>>>>>>>> {task}")
+        task_result = evaluate_dataset(wrapped_model, **task)
+        results.append(task_result)
+        print(task_result)
+    results = pd.DataFrame(results)
+    print(results)
