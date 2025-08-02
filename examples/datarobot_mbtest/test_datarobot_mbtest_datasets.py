3a7fc2f3914d70c47de09a7b28ec43f7bf5c74bf
Lingjun Kang <lyndon.kang@datarobot.com>
2025-08-01 20:53:22 -0400

    add supports of testing DR mbtest datasets

---
+++ examples/datarobot_mbtest/test_datarobot_mbtest_datasets.py
@@ -0,0 +1,84 @@
+
+import os
+import pytest
+from pathlib import Path
+from typing import Iterator
+from typing import List
+
+from tirex import load_model
+
+from dr_model_benchmark.common.analysis.entities import ModelScoreMetrics
+from dr_model_benchmark.common.analysis.entities import ModelTimeProfiles
+from dr_model_benchmark.common.analysis.entities import TestResultV2
+from dr_model_benchmark.common.analysis.enums import Partition
+from dr_model_benchmark.common.enums import MetricType
+from dr_model_benchmark.common.profile.entities import TimeProfile
+from dr_model_benchmark.common.profile.utils import TimeProfiler
+from dr_model_benchmark.common.profile.enums import TimeProfileType
+
+from data import TestDataset
+
+
+import pandas as pd
+from utils import TiRexGiftEvalWrapper, evaluate_dataset
+
+
+@pytest.fixture(scope="module", autouse=True)
+def set_gift_eval_path() -> Iterator[None]:
+    os.environ["GIFT_EVAL"] = "/home/lkanggithub/projects/foundation_model_compare/gift_eval_datasets/"
+    yield
+
+
+@pytest.fixture
+def test_result_output_path() -> Path:
+    return Path(
+        "/home/lkanggithub/projects/foundation_model_compare/"
+        "results_datarobot_mbtest_datasets_tirex_gpu_with_time.csv"
+    )
+
+
+def test(test_result_output_path: Path) -> None:
+    model = load_model("NX-AI/TiRex", device="cuda:0")
+    wrapped_model = TiRexGiftEvalWrapper(model)
+    test_results: List[TestResultV2] = []
+    datarobot_mbtest_yaml_path = Path(
+        "/home/lkanggithub/projects/foundation_model_compare/"
+        "custom_data_no_pii_ts_with_lab_machine_path.yaml"
+    )
+    time_series_frequence = "D"
+    test_datasets = TestDataset.create_from_datarobot_mbtest_yaml(
+        datarobot_mbtest_yaml_path, time_series_frequence, True,
+    )
+    for test_dataset in test_datasets:
+        print(f">>>>>>>>>>>>>>>>> {test_dataset.name}")
+        partition = Partition.TEST
+        test_time_profile = TimeProfile(partition.name)
+        with TimeProfiler(test_time_profile):
+            task_result = evaluate_dataset(wrapped_model, test_dataset)
+        model_score_metrics = [
+            ModelScoreMetrics(
+                MetricType.MAE,
+                partition=partition,
+                score=float(task_result["eval_metrics/MAE[0.5]"]),
+            ),
+            ModelScoreMetrics(
+                MetricType.MAPE,
+                partition=partition,
+                score=float(task_result["eval_metrics/MAPE[0.5]"]),
+            ),
+        ]
+        model_time_profiles = [
+            ModelTimeProfiles(
+                TimeProfileType.TOTAL_CLOCK_TIME,
+                partition,
+                test_time_profile.time_ellipse,
+            )
+        ]
+        test_result = TestResultV2(
+            dataset_name=test_dataset.name,
+            model_score_metrics=model_score_metrics,
+            model_time_profiles=model_time_profiles,
+        )
+        test_results.append(test_result)
+        print(task_result)
+    TestResultV2.to_csv(test_results, test_result_output_path)
---
+++ examples/datarobot_mbtest/utils.py
@@ -0,0 +1,114 @@
+import logging
+from dataclasses import dataclass
+from typing import Any
+
+from gluonts.ev.metrics import (
+    MAE,
+    MAPE,
+    MASE,
+    MSE,
+    MSIS,
+    ND,
+    NRMSE,
+    RMSE,
+    SMAPE,
+    MeanWeightedSumQuantileLoss,
+)
+from gluonts.model import evaluate_model
+from gluonts.time_feature import get_seasonality
+
+from examples.datarobot_mbtest.data import TestDataset
+
+
+# avoid excessive logging
+class WarningFilter(logging.Filter):
+    def __init__(self, text_to_filter):
+        super().__init__()
+        self.text_to_filter = text_to_filter
+
+    def filter(self, record):
+        return self.text_to_filter not in record.getMessage()
+
+
+gts_logger = logging.getLogger("gluonts.model.forecast")
+gts_logger.addFilter(WarningFilter("The mean prediction is not stored in the forecast data"))
+
+METRICS = [
+    MSE(forecast_type="mean"),
+    MSE(forecast_type=0.5),
+    MAE(),
+    MASE(),
+    MAPE(),
+    SMAPE(),
+    MSIS(),
+    RMSE(),
+    NRMSE(),
+    ND(),
+    MeanWeightedSumQuantileLoss(quantile_levels=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]),
+]
+
+
+# Setup GiftEval evaluation
+def evaluate_dataset(predictor, dataset: TestDataset):
+    predictor.set_prediction_len(dataset.prediction_length)
+    predictor.set_ds_freq(dataset.time_series_frequency)
+    predictor.set_context_len(dataset.prediction_length)  # FIXME
+    season_length = get_seasonality(dataset.time_series_frequency)
+
+    # Measure the time taken for evaluation
+    res = evaluate_model(
+        predictor,
+        test_data=dataset.test_data,
+        metrics=METRICS,
+        batch_size=1024,
+        axis=None,
+        mask_invalid_label=True,
+        allow_nan_forecast=False,
+        seasonality=season_length,
+    )
+    result = {
+        "dataset": dataset.name,
+        "model": predictor.model_id,
+        "eval_metrics/MSE[mean]": res["MSE[mean]"][0],
+        "eval_metrics/MSE[0.5]": res["MSE[0.5]"][0],
+        "eval_metrics/MAE[0.5]": res["MAE[0.5]"][0],
+        "eval_metrics/MASE[0.5]": res["MASE[0.5]"][0],
+        "eval_metrics/MAPE[0.5]": res["MAPE[0.5]"][0],
+        "eval_metrics/sMAPE[0.5]": res["sMAPE[0.5]"][0],
+        "eval_metrics/MSIS": res["MSIS"][0],
+        "eval_metrics/RMSE[mean]": res["RMSE[mean]"][0],
+        "eval_metrics/NRMSE[mean]": res["NRMSE[mean]"][0],
+        "eval_metrics/ND[0.5]": res["ND[0.5]"][0],
+        "eval_metrics/mean_weighted_sum_quantile_loss": res["mean_weighted_sum_quantile_loss"][0],
+    }
+    return result
+
+
+@dataclass
+class TiRexGiftEvalWrapper:
+    model: Any
+    freq: str = None
+    pred_len: int = 32
+    context_len: int = 128
+
+    def set_ds_freq(self, freq):
+        self.freq = freq
+
+    def set_prediction_len(self, pred_len):
+        self.pred_len = pred_len
+
+    def set_context_len(self, context_len: int) -> None:
+        self.context_len = context_len
+
+    def predict(self, test_data_input):
+        forecasts = self.model.forecast_gluon(
+            test_data_input,
+            prediction_length=self.pred_len,
+            output_type="gluonts",
+            predict_context_length=self.context_len,
+        )
+        return forecasts
+
+    @property
+    def model_id(self):
+        return "TiRex"
